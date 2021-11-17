c32ee8c590e2e5b8e0b520ad592ec3b493d8536c
kaushal waghela <kaushalwaghela1999@gmail.com>
2021-11-17 21:56:10 +0530

    modified app.js and added a new file
    Added helpers\routes.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from "./containers/footer";
 import { FaqsContainer } from "./containers/faqs";
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
+  const user = null;
   return(
     <>
     <JumbotronContainer />
