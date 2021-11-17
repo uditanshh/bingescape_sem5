b0e727e4171ca3942bf2bfb097b5f38f59c751fd
Uditansh Patel <udit.patel2526@gmail.com>
2021-11-17 22:55:51 +0530

    Added Loading component.
    Added the Loading component and made the required changes in the index.js file for the components folder.

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export {default as Footer} from './footer';
 export {default as Accordion} from './accordion';
 export { default as Feature } from './feature';
 export { default as OptForm } from './opt-form';
+export { default as Loading } from './loading';
 export {default as Header} from './header';
