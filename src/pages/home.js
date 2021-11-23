b26cf200965c52b8b59eadd0439d62a6cab3e3aa
Uditansh Patel <udit.patel2526@gmail.com>
2021-11-24 01:10:47 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,8 +10,8 @@ export default function Home() {
     <>
       <HeaderContainer>
         <Feature>
-          <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
-          <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+          <Feature.Title>Unlimited movies, shows and more, all at your fingertips.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Download at any time.</Feature.SubTitle>
           <OptForm>
             <OptForm.Input placeholder="Email address" />
             <OptForm.Button>Try it now</OptForm.Button>
